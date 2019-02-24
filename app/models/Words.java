5a4e19ed5328ea673f725561342b5d98fa64e322
Louis <lrflrf@sbcglobal.net>
2019-02-24 12:00:23 -0600

    Initial Check In

---
+++ app/models/Words.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Words
+{
+    private String place1;
+
+    public String getPlace1()
+    {
+        return place1;
+    }
+
+    public void setPlace1(String place1)
+    {
+        this.place1 = place1;
+    }
+}
